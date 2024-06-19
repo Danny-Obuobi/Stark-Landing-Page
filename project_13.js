a63c41ef2c05e9498a40f94d12a61e1e82c1030a
Daniel Adu Obuobi <adudaniel131@gmail.com>
2024-06-19 12:27:23 +0000

    Assigned a new class name to the ul tag which had a class name drop-down-list and also a click event listener functionality on new class name

--- project_13.js
+++ project_13.js
@@ -2,7 +2,9 @@
 
 
 const my_button = document.querySelector('.arrow-image')
+const dropdownList = document.querySelector('.drop-down-list')
 
 my_button.addEventListener('click', () => {
     my_button.classList.toggle("arrow-image-open");
+    dropdownList.classList.toggle("drop-down-visible");
 });
